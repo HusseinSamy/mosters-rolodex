9f23fbfe482c5556fc253f1ceac6653af5056abd
Hussein Samy <hussein.samy02@gmail.com>
2023-06-06 21:53:31 +0200

    Adds search component

---
+++ src/components/search-input/searchInput.component.jsx
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import './search-box.styles.css'
+class SearchInput extends Component {
+    render() {
+        return (
+            <input
+            className={`search-box ${this.props.className}`}
+            type='search'
+            placeholder={this.props.placeholder}
+            onChange={this.props.onChangeHandler} />
+        )
+    }
+}
+export default SearchInput;
