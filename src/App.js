ee21beaa7fc0ae06f630c7454c688fe040793bc4
Hussein Samy <hussein.samy02@gmail.com>
2023-06-06 21:54:44 +0200

    Splits app into multiple components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/cardList.component';
+import SearchInput from './components/search-input/searchInput.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Hussein'
-    };
-   
+      monsters: [],
+      filteredMonsters: []
+    }
   }
+
+  fetchMonsters() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+      return response.json();
+    })
+    .then((monsters) => {
+      this.setState(() => {
+        return {
+          monsters: monsters,
+          filteredMonsters: monsters,
+        };
+      });
+  })
+  }
+  componentDidMount() {
+    this.fetchMonsters();
+  }
+
+  search = (event) => {
+    let filteredList = this.state.monsters.filter((monster) => (
+      monster.name.toLowerCase())
+      .includes(
+        (event.target.value).toLowerCase()
+      ))
+
+      this.setState({ filteredMonsters: filteredList })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi! {this.state.name}
-          </p>
-          <button onClick={() => this.setState({ name: 'New Name, assisted by chatGPT' }, ()=>{alert('new name has been set!')})}>Press to change name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <SearchInput placeholder="Search for a monster" className='searchBox' onChangeHandler={this.search} />
+        <CardList filteredMonsters={this.state.filteredMonsters} />
       </div>
     );
   }
